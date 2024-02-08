2d3bfb8afc31cfa0b454d92be0ffc2cb9b5ab438
Julia Fout <juuuulash@icloud.com>
2024-02-07 23:59:00 -0600

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,16 @@
 const express = require('express');
 const router = express.Router();
 
+
 //TEST ROUTE//
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
