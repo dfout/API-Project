8059fcfd858afeeb67fbcd65851280520ef86397
Julia Fout <juuuulash@icloud.com>
2024-02-10 14:19:29 -0800

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,9 +4,34 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
-router.post('/',async (req,res)=>{
+// Middleware for validating sign up.
+
+const validateSignup = [
+    check('email')
+        .exists({checkFalsy:true})
+        .isEmail()
+        .withMessage('Please provide a valid email'),
+    check('username')
+        .exists({checkFalsy:true})
+        .isLength({min:4})
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email'),
+    check('password')
+        .exists({checkFalsy:true})
+        .isLength({min:6})
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
+
+router.post('/', validateSignup, async (req,res)=>{
     const { email, password, username } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
     const user = await User.create ({ email, username, hashedPassword });
@@ -24,6 +49,79 @@ router.post('/',async (req,res)=>{
     });
 
 })
+
+//*Passed
+// fetch('/api/users', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": "VCx0cXFq-GIkR_UUo5hmcVrtTGYgJxIR2biQ"
+//     },
+//     body: JSON.stringify({
+//       email: 'firestar@spider.man',
+//       username: 'Firestar',
+//       password: ''
+//     })
+//   }).then(res => res.json()).then(data => console.log(data));
+
+
+//*Email is not an email
+// fetch('/api/users', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": "VCx0cXFq-GIkR_UUo5hmcVrtTGYgJxIR2biQ"
+//     },
+//     body: JSON.stringify({
+//       email: 'firestarspider.man',
+//       username: 'Firestar',
+//       password: 'isthisaproperpass'
+//     })
+//   }).then(res => res.json()).then(data => console.log(data));
+
+
+//* Username is only 3 characters long
+// fetch('/api/users', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": "VCx0cXFq-GIkR_UUo5hmcVrtTGYgJxIR2biQ"
+//     },
+//     body: JSON.stringify({
+//       email: 'firestarspider.man',
+//       username: 'Fir',
+//       password: 'isthisaproperpass'
+//     })
+//   }).then(res => res.json()).then(data => console.log(data));
+
+//* Username field is an email
+// fetch('/api/users', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": "VCx0cXFq-GIkR_UUo5hmcVrtTGYgJxIR2biQ"
+//     },
+//     body: JSON.stringify({
+//       email: 'firestar@spider.man',
+//       username: 'firestar@spider.man',
+//       password: 'isthisaproperpass'
+//     })
+//   }).then(res => res.json()).then(data => console.log(data));
+
+
+//* password field is only 5 characters long
+// fetch('/api/users', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": "VCx0cXFq-GIkR_UUo5hmcVrtTGYgJxIR2biQ"
+//     },
+//     body: JSON.stringify({
+//       email: 'firestar@spider.man',
+//       username: 'Firestar',
+//       password: 'ottff'
+//     })
+//   }).then(res => res.json()).then(data => console.log(data));
 
 //!TEST
 // fetch('/api/users', {
