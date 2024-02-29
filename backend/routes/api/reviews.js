3fef94c938de8374ef0cf9c02a406576bf67a0fa
Julia Fout <juuuulash@icloud.com>
2024-02-28 18:55:51 -0800

    Finished POST /:reviewId/images

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -8,6 +8,17 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+function deepAuth(userId,review){
+    //if the userId matches the ownerId on the spot,
+    //then send back a true
+    if (review.userId === userId) return true
+    else{
+        return false
+    }
+
+    //else, send back false then send an error message
+}
+
 router.get('/current', requireAuth, async(req,res,next)=>{
     const userId = req.user.dataValues.id;
     const userReviews = {}
@@ -24,6 +35,39 @@ router.get('/current', requireAuth, async(req,res,next)=>{
 
 })
 
+router.post('/:reviewId/images', requireAuth, async (req,res,next)=>{
+    const { currId } = req.user.dataValues.id;
+    const { reviewId } = req.params;
+    const review = Review.findByPk(reviewId);
+    if(!review){
+        res.status(404);
+        return res.json({
+            message:"Review couldn't be found"
+        })
+    }
+
+    if(!deepAuth(currId,review)){
+        res.status(403);
+        return res.json({
+            message: "Forbidden"
+        })
+    };
+
+    const reviewImages = ReviewImage.findAll({
+        where: {reviewId:reviewId}
+    });
+
+    if (reviewImages.length === 10){
+        res.status(403)
+        return res.json({
+            message:"Maximum number of images for this resource was reached"
+        })
+    };
+
+    const newReviewImage = await ReviewImage.create(req.body)
+    res.status(200);
+    return res.json(newReviewImage)
+})
 
 
 
