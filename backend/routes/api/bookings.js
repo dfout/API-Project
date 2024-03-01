981bcdc5339898161e9b8742bcd8e4d3393b318b
Julia Fout <juuuulash@icloud.com>
2024-02-29 20:48:24 -0800

    Finished get bookings from curr user

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -2,8 +2,22 @@ const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs')
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
-const { User, Review, ReviewImage, Spot } = require('../../db/models')
+const { User, Review, ReviewImage, Spot, Booking} = require('../../db/models')
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+
+// Return all the bookings that the current user has made
+router.get('/current',requireAuth, async(req,res,next)=>{
+    const userId = req.user.id;
+    const currBookings = await Booking.findAll({
+        where:{userId:userId},
+        include:{model: "Spots"}
+
+    })
+    return res.json({Bookings:currBookings})
+})
+
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,6 +7,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
+const bookingsRouter = require('./bookings.js')
 
 
 
@@ -15,6 +16,7 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots',spotsRouter)
 router.use('/reviews', reviewsRouter);
+router.use('/bookingsRouter', bookingsRouter)
 
 router.post('/test', (req,res)=>{
   res.json({requestBody:req.body});
