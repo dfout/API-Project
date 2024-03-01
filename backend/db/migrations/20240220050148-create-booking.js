34cb380f0181f7bcb1834f7e99f540870e1326f0
Julia Fout <juuuulash@icloud.com>
2024-02-29 17:05:11 -0800

    added options obj to dropTable

--- backend/db/migrations/20240220050148-create-booking.js
+++ backend/db/migrations/20240220050148-create-booking.js
@@ -52,6 +52,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Bookings'
-    await queryInterface.dropTable('Bookings');
+    await queryInterface.dropTable(options);
   }
 };
--- backend/db/migrations/20240220050636-create-review.js
+++ backend/db/migrations/20240220050636-create-review.js
@@ -55,6 +55,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Reviews'
-    await queryInterface.dropTable('Reviews');
+    await queryInterface.dropTable(options);
   }
 };
