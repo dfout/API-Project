2a529419cede63b9b91aa364291607fcdb5e7bfb
Drew Fout <drewmf1566@gmail.com>
2024-04-06 17:28:05 -0500

    Finished Build/Front-End

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -22,9 +22,9 @@ router.use('/bookings', bookingsRouter);
 router.use('/spot-images', spotImageRouter);
 router.use('/review-images',reviewImageRouter);
 
-router.post('/test', (req,res)=>{
-  res.json({requestBody:req.body});
-})
+// router.post('/test', (req,res)=>{
+//   res.json({requestBody:req.body});
+// })
 
 
 
